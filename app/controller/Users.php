02e0738a7a857053616e591b7a4e221a3265cd49
AshleighTempleton <ashleigh.ann.templeton@gmail.com>
2020-10-27 17:40:55 +0100

    working login & session

--- app/controller/Users.php
+++ app/controller/Users.php
@@ -66,6 +66,7 @@ class Users extends Controller{
 
                 // register user
                 if($this->userModel->register($data)){
+                    flash('register_success', 'You have been registered');
                     redirect('users/login');
                 } else {
                     die('Something went wrong :(');
@@ -126,10 +127,29 @@ class Users extends Controller{
                     $data['password_err'] = 'Please enter your password';
                 } 
 
+
+            // check user exists
+            if($this->userModel->findUserByEmail($data['email'])){
+                //user found
+            } else {
+                $data['email_err'] = 'No user found';
+            }
+
+
             // ensure errors are empty
             if(empty($data['email_err']) && empty($data['password_err'])){
                 // validated
-                die('SUCCESS');
+                // check and set logged in user
+                $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+                
+                if($loggedInUser){
+                    // create session
+                    $this->createUserSession($loggedInUser);
+                } else {
+                    $data['password_err'] = 'Password incorrect';
+                    $this->view('users/login', $data);
+                }
+
             } else {
                 // load view with errors
                 $this->view('users/login', $data);
@@ -153,5 +173,13 @@ class Users extends Controller{
             $this->view('users/login', $data);
 
         }
+    }
+
+    public function createUserSession($user){
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_email'] = $user->email;
+        $_SESSION['user_userName'] = $user->userName;
+        redirect('pages/index');
+
     }
 }
