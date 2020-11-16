3917f0dc19a821d6ba510c8b1ca04e385a7aa627
AshleighTempleton <ashleigh.ann.templeton@gmail.com>
2020-11-16 01:00:44 +0100

    updated frontend

--- app/view/pages/index.php
+++ app/view/pages/index.php
@@ -9,11 +9,15 @@
             <!-- Jumbotron -->
 
 
-            <div class="jumbotron text-white bg-dark jumboText">
+            <div class="jumbotron text-light bg-dark jumboText">
                     <div class="jumboText">
-                        <h1 class=""><?php echo $data['title']; ?></h1>
-                        <p class="lead">"Do you get to the cloud district very often? What am I saying... Of course you don't."</p>
-                        <p class="lead"><a href="#nav">
+                        <h1 id="title"><?php echo $data['title']; ?></h1>
+                        <div id="subtitle">
+                        <p>Do you get to the cloud district very often?</p>
+                        <p>Oh what am I saying... Of course you don't.</p>
+
+                        </div>
+                        <p><a href="#nav" id="continue" class="text-light">
                             <i class="far fa-arrow-alt-circle-down"></i> Continue the adventure <i class="far fa-arrow-alt-circle-down">
                             </a></i></p>
                     </div>
@@ -22,137 +26,39 @@
                                         <?php require_once "../app/view/inc/nav.php" ?>
         </header>
 
-    <!-- <div class="outerContainer container-fluid"> -->
 
 
-        <div class="row" id="main">
+        <div class="row my-5">
             <div class="col-12 col-lg-8 d-flex justify-content-center align-items-center">
                     <div class="card main-card">
                         <div class="card-body">
-                            <h5 class="card-title">Console wars reignited!</h5>
-                            <p class="card-text">Who will win the battle between the PS5 and Xbox Series X?</p>
-                            <p class="card-text">PC master race of course.</p>
-                            <a href="https://www.techradar.com/news/ps5-vs-xbox-series-x-which-next-gen-console-should-you-buy" class="btn btn-primary">Read the full article</a>
+                            <div class="overlay text-center">
+
+                                <h5 class="card-title">Console wars reignited!</h5>
+                                <p class="card-text">Who will win the battle between the PS5 and Xbox Series X?</p>
+                                <p class="card-text">PC master race of course.</p>
+                                <a href="https://www.techradar.com/news/ps5-vs-xbox-series-x-which-next-gen-console-should-you-buy" class="overlayLink">Read the full article</a>
+                            </div>
                         </div>
                     </div>
-                
-                <!-- <div class="card bg-dark text-white main-card"> -->
-                    <!-- <img src="images/sample.jpg" class="card-img" alt="..."> -->
-                    <!-- <div class="card-img-overlay">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text">Last updated 3 mins ago</p>
-                    </div> -->
-                <!-- </div> -->
             </div>
             <div class="col-12 col-lg-4 d-flex justify-content-center align-items-center">
                 
                 <?php require_once "../app/view/inc/weather.php" ?>
             </div>
 
-
-
-            <!-- TOP STORIES -->
-
-            <!-- <div class="topStories row"> -->
-                <!-- ONE -->
-                <!-- <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center">
-                    <div class="card top-stories mass"> -->
-                        <!-- <img src="images/me.jpg" class="card-img-top" alt="..."> -->
-                        <!-- <div class="card-body">
-                            <h5 class="card-title">For I am your Shepard</h5>
-                            <p class="card-text">The next chapter is coming.. and a trilogy remaster!</p>
-                            <a href="https://www.pcgamer.com/the-new-mass-effects-teaser-image-references-both-mass-effect-2-and-andromeda/" class="btn btn-primary">Read the full article</a>
-                        </div>
-                    </div>
-                </div> -->
-
-                <!-- TWO -->
-                <!-- <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center">
-                    <div class="card top-stories ac"> -->
-                        <!-- <img src="images/AC.jpg" class="card-img-top" alt="..."> -->
-                        <!-- <div class="card-body">
-                            <h5 class="card-title">Praise Odin!</h5>
-                            <p class="card-text">Assassin's Creed Valhalla's extensive options should be the new standard for PC gaming</p>
-                            <a href="https://www.pcgamer.com/assassins-creed-valhallas-extensive-options-should-be-the-new-standard-for-pc-gaming/" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div> -->
-
-                <!-- THREE -->
-                <!-- <div class="col-12 col-lg-4 d-flex justify-content-center align-items-center">
-                    <div class="card top-stories yakuza"> -->
-                        <!-- <img src="images/yakuza.jpg" class="card-img-top" alt="Yakuza"> -->
-                        <!-- <div class="card-body">
-                            <h5 class="card-title">Like a Dragon</h5>
-                            <p class="card-text">A fresh start for the Yakuza series?</p>
-                            <a href="https://www.theverge.com/21558093/yakuza-like-a-dragon-review-xbox-series-x"" class="btn btn-primary">More here</a>
-                        </div>
-                    </div>
-                </div>
-
-
-
-            <div class="row">
-                <div class="col-12 col-lg-6">
-                    <div class="row">
-                        <div class="col-6">
-                            <h3>Like a dragon</h3>
-                            <p>A fresh start for the Yakuza series?</p>
-                        </div>
-                        <div class="col-4">
-                            <a href="https://www.theverge.com/21558093/yakuza-like-a-dragon-review-xbox-series-x">
-                            <img src="images/yakuza.jpg" alt="yakuza like a dragon" class="img-small">
-                        </a>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-8">
-                            <h3>THIS IS A STORY</h3>
-                            <p>THIS IS SOME TEXT</p>
-                        </div>
-                        <div class="col-4">
-                            <img src="" alt="sample">
-                        </div>
-                    </div>
-                    <hr>
-                </div>
-                <div class="col-12 col-lg-6">
-                    <div class="row">
-                        <div class="col-8">
-                            <h3>THIS IS A STORY</h3>
-                            <p>THIS IS SOME TEXT</p>
-                        </div>
-                        <div class="col-4">
-                            <img src="" alt="sample">
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-8">
-                            <h3>THIS IS A STORY</h3>
-                            <p>THIS IS SOME TEXT</p>
-                        </div>
-                        <div class="col-4">
-                            <img src="" alt="sample">
-                        </div>
-                    </div>
-                    <hr>
-                </div>
-
-
-            </div> -->
-
+<!-- owl carousel -->
 
             <div class="owl-carousel owl-theme">
                 <div class="item">
                     <div class="card top-stories mass">
                         <!-- <img src="images/me.jpg" class="card-img-top" alt="..."> -->
                         <div class="card-body">
-                            <h5 class="card-title">For I am your Shepard</h5>
-                            <p class="card-text">The next chapter is coming.. and a trilogy remaster!</p>
-                            <a href="https://www.pcgamer.com/the-new-mass-effects-teaser-image-references-both-mass-effect-2-and-andromeda/" class="btn btn-primary">Read the full article</a>
+                            <div class="overlay text-center">
+                                <h5 class="card-title">For I am your Shepard</h5>
+                                <p class="card-text">The next chapter is coming.. and a trilogy remaster!</p>
+                                <a href="https://www.pcgamer.com/the-new-mass-effects-teaser-image-references-both-mass-effect-2-and-andromeda/" class="overlayLink">Read more</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -160,9 +66,11 @@
                     <div class="card top-stories ac">
                         <!-- <img src="images/AC.jpg" class="card-img-top" alt="..."> -->
                         <div class="card-body">
-                            <h5 class="card-title">Praise Odin!</h5>
-                            <p class="card-text">Assassin's Creed Valhalla's extensive options should be the new standard for PC gaming</p>
-                            <a href="https://www.pcgamer.com/assassins-creed-valhallas-extensive-options-should-be-the-new-standard-for-pc-gaming/" class="btn btn-primary">Go somewhere</a>
+                            <div class="overlay text-center">
+                                <h5 class="card-title">Praise Odin!</h5>
+                                <p class="card-text">Assassin's Creed Valhalla's extensive options should be the new standard for PC gaming</p>
+                                <a href="https://www.pcgamer.com/assassins-creed-valhallas-extensive-options-should-be-the-new-standard-for-pc-gaming/" class="overlayLink">Read more</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -170,9 +78,11 @@
                     <div class="card top-stories yakuza">
                         <!-- <img src="images/yakuza.jpg" class="card-img-top" alt="Yakuza"> -->
                         <div class="card-body">
-                            <h5 class="card-title">Like a Dragon</h5>
-                            <p class="card-text">A fresh start for the Yakuza series?</p>
-                            <a href="https://www.theverge.com/21558093/yakuza-like-a-dragon-review-xbox-series-x"" class="btn btn-primary">More here</a>
+                            <div class="overlay text-center">
+                                <h5 class="card-title">Like a Dragon</h5>
+                                <p class="card-text">A fresh start for the Yakuza series?</p>
+                                <a href="https://www.theverge.com/21558093/yakuza-like-a-dragon-review-xbox-series-x" class="overlayLink">Read more</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -180,9 +90,11 @@
                     <div class="card top-stories radeon">
                             <!-- <img src="images/me.jpg" class="card-img-top" alt="..."> -->
                             <div class="card-body">
-                                <h5 class="card-title">Things we cannot afford</h5>
-                                <p class="card-text">AMD's Radeon RX 6900 XT said to be aiming at the Nvidia RTX 3090</p>
-                                <a href="https://www.pcgamer.com/the-new-mass-effects-teaser-image-references-both-mass-effect-2-and-andromeda/" class="btn btn-primary">Read the full article</a>
+                                <div class="overlay text-center">
+                                    <h5 class="card-title">Things we cannot afford</h5>
+                                    <p class="card-text">AMD's Radeon RX 6900 XT said to be aiming at the Nvidia RTX 3090</p>
+                                    <a href="https://www.pcgamer.com/the-new-mass-effects-teaser-image-references-both-mass-effect-2-and-andromeda/" class="overlayLink">Read more</a>
+                                </div>
                             </div>
                         </div>
                 </div>
@@ -190,9 +102,11 @@
                     <div class="card top-stories watchDogs">
                         <!-- <img src="images/me.jpg" class="card-img-top" alt="..."> -->
                         <div class="card-body">
-                            <h5 class="card-title">An underwhelming legion</h5>
-                            <p class="card-text">Watch Dogs: Legion not exactly perfection</p>
-                            <a href="https://www.a90skid.com/watch-dogs-legion-review/" class="btn btn-primary">Read the full article</a>
+                            <div class="overlay text-center">
+                                <h5 class="card-title">An underwhelming legion</h5>
+                                <p class="card-text">Watch Dogs: Legion not exactly perfection</p>
+                                <a href="https://www.a90skid.com/watch-dogs-legion-review/" class="overlayLink">Read more</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -200,9 +114,11 @@
                     <div class="card top-stories witcher">
                         <!-- <img src="images/me.jpg" class="card-img-top" alt="..."> -->
                         <div class="card-body">
-                            <h5 class="card-title">Oh Valley of plenty ~</h5>
-                            <p class="card-text">The Witcher 3 is being updated for the next gen!</p>
-                            <a href="https://en.cdprojektred.com/news/the-witcher-3-wild-hunt-is-coming-to-the-next-generation/" class="btn btn-primary">Read the full article</a>
+                            <div class="overlay text-center">
+                                <h5 class="card-title">Oh Valley of plenty ~</h5>
+                                <p class="card-text">The Witcher 3 is being updated for the next gen!</p>
+                                <a href="https://en.cdprojektred.com/news/the-witcher-3-wild-hunt-is-coming-to-the-next-generation/" class="overlayLink">Read more</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -210,9 +126,11 @@
                     <div class="card top-stories souls">
                         <!-- <img src="images/me.jpg" class="card-img-top" alt="..."> -->
                         <div class="card-body">
-                            <h5 class="card-title">Can it get any better?</h5>
-                            <p class="card-text">Demon's Souls 2020 is an improvement in every way.</p>
-                            <a href="https://www.polygon.com/reviews/2020/11/13/21564203/demons-souls-ps5-review-remake-bluepoint-games-playstation-5" class="btn btn-primary">Read the full article</a>
+                            <div class="overlay text-center">
+                                <h5 class="card-title">Can it get any better?</h5>
+                                <p class="card-text">Demon's Souls 2020 is an improvement in every way.</p>
+                                <a href="https://www.polygon.com/reviews/2020/11/13/21564203/demons-souls-ps5-review-remake-bluepoint-games-playstation-5" class="overlayLink">Read more</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -220,19 +138,23 @@
                     <div class="card top-stories zombie">
                         <!-- <img src="images/me.jpg" class="card-img-top" alt="..."> -->
                         <div class="card-body">
-                            <h5 class="card-title">It's Chris Redfield... again</h5>
-                            <p class="card-text">Resident Evil Village is coming in 2021</p>
-                            <a href="https://www.techradar.com/news/resident-evil-8-release-date-news-and-trailers" class="btn btn-primary">Read the full article</a>
+                            <div class="overlay text-center">
+                                <h5 class="card-title">It's Chris Redfield... again</h5>
+                                <p class="card-text">Resident Evil Village is coming in 2021</p>
+                                <a href="https://www.techradar.com/news/resident-evil-8-release-date-news-and-trailers" class="overlayLink">Read more</a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="item">
                     <div class="card top-stories solas">
                         <!-- <img src="images/me.jpg" class="card-img-top" alt="..."> -->
-                        <div class="card-body">
-                            <h5 class="card-title">I suspect you have questions</h5>
-                            <p class="card-text">The latest news in our journey to Tevinter</p>
-                            <a href="https://www.techradar.com/news/dragon-age-4-release-date-news-and-rumors" class="btn btn-primary">Read the full article</a>
+                        <div class="card-body"> 
+                            <div class="overlay text-center">
+                                <h5 class="card-title">I suspect you have questions</h5>
+                                <p class="card-text">The latest news in our journey to Tevinter</p>
+                                <a href="https://www.techradar.com/news/dragon-age-4-release-date-news-and-rumors" class="overlayLink">Read more</a>
+                            </div>
                         </div>
                     </div>
                 </div>
